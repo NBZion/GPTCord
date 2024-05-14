fdfcbf4d9d7e2ac64a4d018b3d7c1083714ed69d
NBZion <subtopewds0921@gmail.com>
2024-05-15 00:39:01 +0800

    Add Ping Command

--- cogs/gpt.py
+++ cogs/gpt.py
@@ -1,14 +1,27 @@
 import discord
 from discord.ext import commands
 
+## Whitelisted Servers 
+whitelistedServers=[1053879369011044444]
+
 class gpt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author != self.bot.user:  
-            await message.channel.send("Hi!")
+    async def on_message(self, ctx):
+        if ctx.author != self.bot.user:  
+            print(ctx)
+
+    @commands.slash_command(guild_ids = whitelistedServers,description="Ping!")
+    async def ping(self, ctx):
+        await ctx.respond(f"Pong!({self.bot.latency} ms)")
+
+    @commands.slash_command(guild_ids = whitelistedServers, description="Ask GPT a Question!")
+    async def ask(self,message):
+        print("Hello World!")
+
+    
 
 
 def setup(bot):
